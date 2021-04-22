243524ab50846ccb6172f6b3f85aaf315b012f39
Andreas <201604990@post.au.dk>
2021-04-22 11:05:48 +0200

    done

--- Assignment3/Task3.c
+++ Assignment3/Task3.c
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include "arch/dev/radio/cc2420/cc2420.h"
 
-PROCESS(rssiSIG, "DCT signal thingy");
+PROCESS(rssiSIG, "RSSI reading");
 AUTOSTART_PROCESSES(&rssiSIG);
 
 #define LOG_MODULE "Test"
@@ -29,31 +29,42 @@ PROCESS_THREAD(rssiSIG, ev, data)
 	
 	int rssi = 0;
 	int channel = 0;
+    int i = 0;
+    float rssiMean = 0;
+    int samples = 300;
     
     static struct etimer starTime;
-    
 
         PROCESS_BEGIN();
 
             SENSORS_ACTIVATE(button_sensor);
             SENSORS_ACTIVATE(light_sensor);
 			
-			int cc2420_on();
-            			
+			cc2420_on();
+            
             while(1)
             {
-				
-				for(channel = 11; channel<27; channel++)
-				{
-					cc2420_set_channel(channel);
-					rssi = cc2420_rssi();
-					printf("The RSSI value for channel %d is: %d\n", channel, rssi);
-				}
-				
-				etimer_set(&starTime, CLOCK_SECOND*30);
-				PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&starTime));
+				for (channel = 11; channel<27; channel++)
+                {
+                    rssiMean = 0;
+                    for(i = 0; i < samples; i++)
+                    {
+                        cc2420_set_channel(channel);
+                        rssi = cc2420_rssi();
+                        rssiMean += rssi;
+                    }
+                    rssiMean = rssiMean/samples;
+                    int A;
+                    float frac;
+                    A=rssiMean; // get integral part, i.e. A=3
+                    frac=(rssiMean-A)*100; // get fractional part, i.e. frac=55
+                    printf("RSSI mean value: %d.%02u for channel %d\n", A, (unsigned int)frac, channel);
+                }
+                etimer_set(&starTime, CLOCK_SECOND*60);
+                PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&starTime));
             }
-		int cc2420_off();
+        
+		cc2420_off();
         SENSORS_DEACTIVATE(button_sensor);
         SENSORS_DEACTIVATE(light_sensor);
         PROCESS_END();
